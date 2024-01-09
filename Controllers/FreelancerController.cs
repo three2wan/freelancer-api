5b523b63622fc38dd81d0f63c21160a1f092ec95
Syazwan <syazwan@pertamadigital.com>
2024-01-09 14:00:20 +0800

    Create, update and delete func freelance

--- Controllers/FreelancerController.cs
+++ Controllers/FreelancerController.cs
@@ -22,6 +22,59 @@ public class FreelancerController : ControllerBase
     [HttpGet("{id}")]
     public async Task<ActionResult<Freelancer>> GetFreelancer(Guid id)
     {
-        return await _context.Freelancers.FindAsync(id);
+        var fr = await _context.Freelancers.FindAsync(id);
+
+        if (fr == null)
+        {
+            return NotFound();
+        }
+
+        return fr;
     }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Freelancer>> UpdateFreelancer(Guid id, Freelancer freelancer)
+    {
+        var fr = await _context.Freelancers.FindAsync(id);
+
+        if (fr == null)
+        {
+            return NotFound();
+        }
+
+        fr.Username = freelancer.Username ?? fr.Username;
+        fr.Email = freelancer.Email ?? fr.Email;
+        fr.Phone = freelancer.Phone ?? fr.Phone;
+        fr.Skillset = freelancer.Skillset ?? fr.Skillset;
+        fr.Hobby = freelancer.Hobby ?? fr.Hobby;
+
+        await _context.SaveChangesAsync();
+
+        return Ok();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Freelancer>> CreateFreelancer([FromBody] Freelancer freelancer)
+    {
+        _context.Freelancers.Add(freelancer);
+        await _context.SaveChangesAsync();
+
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteFreelancer(Guid id)
+    {
+        var fr = await _context.Freelancers.FindAsync(id);
+        if (fr == null)
+        {
+            return NotFound();
+        }
+
+        _context.Freelancers.Remove(fr);
+        await _context.SaveChangesAsync();
+
+        return Ok();
+    }
+
 }
